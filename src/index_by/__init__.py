c0e033664e8e197c09e06eb1f6b26f3e9b93670d
Thomas Wiebe <thomas@gridscale.io>
2019-10-16 12:53:56 +0200

    Provide all functions in pkg top level

---
+++ src/index_by/__init__.py
@@ -0,0 +1,21 @@
+from index_by.attr import index_by_attr, index_by_attr_as_list, index_by_attrs, index_by_attrs_as_list
+from index_by.key import index_by_key, index_by_key_as_list, index_by_keys, index_by_keys_as_list
+from index_by.modifier import index_by_modifier, index_by_modifier_as_list
+from index_by.pop_key import index_by_pop_key, index_by_pop_key_as_list, index_by_pop_keys, index_by_pop_keys_as_list
+
+__all__ = [
+    'index_by_attr',
+    'index_by_attr_as_list',
+    'index_by_attrs',
+    'index_by_attrs_as_list',
+    'index_by_key',
+    'index_by_key_as_list',
+    'index_by_keys',
+    'index_by_keys_as_list',
+    'index_by_pop_key',
+    'index_by_pop_key_as_list',
+    'index_by_pop_keys',
+    'index_by_pop_keys_as_list',
+    'index_by_modifier',
+    'index_by_modifier_as_list'
+]
